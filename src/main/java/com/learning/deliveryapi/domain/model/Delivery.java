44ff6d2ad9f2da5a0716736c28f5ce043a0bfa3c
Caio Antonio <caio.antonio.c@outlook.com>
2022-01-15 18:38:09 -0300

    refactor: updated the delivery request flow

--- src/main/java/com/learning/deliveryapi/domain/model/Delivery.java
+++ src/main/java/com/learning/deliveryapi/domain/model/Delivery.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -21,12 +22,14 @@ public class Delivery {
     @EqualsAndHashCode.Include
     private Long id;
 
+    @NotNull
     @ManyToOne
     private Customer customer;
 
     @Embedded
     private Receiver receiver;
 
+    @NotNull
     @Column(name = "delivery_fee")
     private BigDecimal deliveryFee;
 
