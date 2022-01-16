7a8ffc0d7308cd66e6a981d45489a3a5c697d953
Caio Antonio <caio.antonio.c@outlook.com>
2022-01-15 22:06:14 -0300

    refactor: updated the customer create and delivery request flow

--- src/main/java/com/learning/deliveryapi/api/exception/ExceptionFormat.java
+++ src/main/java/com/learning/deliveryapi/api/exception/ExceptionFormat.java
@@ -11,7 +11,7 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 @Setter
-public class exceptionFormat {
+public class ExceptionFormat {
     private Integer status;
     private OffsetDateTime dateTime;
     private String title;
