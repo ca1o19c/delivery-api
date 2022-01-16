26a2c7b13b4fe1b267003340b719cfb90f6527f6
Caio Antonio <caio.antonio.c@outlook.com>
2022-01-16 15:18:38 -0300

    feat: created the delivery finalize flow

--- src/main/java/com/learning/deliveryapi/domain/service/FinalizeDeliveryService.java
+++ src/main/java/com/learning/deliveryapi/domain/service/FinalizeDeliveryService.java
@@ -1,6 +1,8 @@
 package com.learning.deliveryapi.domain.service;
 
 import com.learning.deliveryapi.domain.repository.DeliveryRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,6 +11,8 @@ public class FinalizeDeliveryService {
 
     public final DeliveryRequestService deliveryRequestService;
     public final DeliveryRepository deliveryRepository;
+
+    Logger logger = LoggerFactory.getLogger(FinalizeDeliveryService.class);
 
     public FinalizeDeliveryService(DeliveryRequestService deliveryRequestService, DeliveryRepository deliveryRepository) {
         this.deliveryRequestService = deliveryRequestService;
@@ -20,6 +24,8 @@ public class FinalizeDeliveryService {
         var delivery = deliveryRequestService.getById(deliveryId);
 
         delivery.finalizeDelivery();
+
+        logger.info("Finalizing delivery with id: {}", delivery.getId());
 
         deliveryRepository.save(delivery);
     }
